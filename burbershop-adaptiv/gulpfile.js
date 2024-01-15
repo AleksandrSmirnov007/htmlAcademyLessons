b20efee76e10d8b724fb2a799cff65b8347cf75e
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-01-16 02:44:19 +0300

    Барбершоп-адаптив Автоматизация написание gulpfile.js
    Скрипт для создания папки build

--- burbershop-adaptiv/gulpfile.js
+++ burbershop-adaptiv/gulpfile.js
@@ -9,6 +9,12 @@ const htmlmin = require("gulp-htmlmin");
 const csso = require("postcss-csso");
 const rename = require("gulp-rename");
 const terser = require("gulp-terser");
+//  при подключении squoosh возникает ошибка (версия node 20.11.0)
+// const squoosh = require("gulp-libsquoosh");
+
+const webp = require('gulp-webp'); // поставилась gulp-webp 5.0.0 с этой версией не работает, поставил 4.0.1 заработало(Иcправил версию в package.json и в консоли написал npm i, пакет откатился до версии указанной посмотреть пакеты и их версии можно командой npm list --depth=0  )
+const svgstore = require('gulp-svgstore'); // поставилась gulp-webp версия 7.*.*  с этой версией не работает, поставил 6.0.0 заработало(Иcправил версию в package.json и в консоли написал npm i, пакет откатился до версии указанной посмотреть пакеты и их версии можно командой npm list --depth=0  )
+const del = require('del');
 
 
 const sync = require('browser-sync').create();
@@ -50,11 +56,68 @@ const scripts = () => {
 
 exports.scripts = scripts;
 
-// images
+// images (squoosh  закоментрован и при функция optimazeImages() не сработает Закоментирую строчку .pipe(squoosh()) изображения на прямую копируются в папку build/image)
+const optimizeImages = () => {
+  return gulp.src("source/image/**/*.{jpg,png,svg}")
+  // .pipe(squoosh())
+  .pipe(gulp.dest("build/image"));
+}
+
+exports.optimizeImages = optimizeImages;
+
+const copyImages = () => {
+  return gulp.src("source/image/**/*.{jpg,png,svg}")
+  .pipe(gulp.dest("build/image"));
+}
+
+exports.copyImages = copyImages;
 
 // Webp
 
+const createWebp = () => {
+  return gulp.src("source/image/**/*.{jpg,png}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/image"));
+}
+
+exports.createWebp = createWebp;
+
 // Sprite
+
+const sprite = () => {
+  return gulp.src("source/image/icon/*.svg")
+  .pipe(svgstore({
+    inlineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe(gulp.dest("build/image"));
+}
+
+exports.sprite = sprite;
+
+// Copy other files
+
+const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/image/**/*.svg",
+    "!source/image/icon/*.svg"
+  ], {
+    base: "source"})
+    .pipe(gulp.dest("build"));
+  done();
+}
+
+exports.copy = copy;
+
+// clean
+
+const clean = () => {
+  return del("build");
+}
+
+exports.clean = clean;
 
 // Server
 
@@ -77,6 +140,24 @@ const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/*.html').on('change', sync.reload);
 }
+
+
+// Build можно обратить внимание: паралельные задачи стоят последовательным блоком в конце последавательных задач
+
+const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  )
+);
+
+exports.build = build;
 
 exports.default = gulp.series(
   styles, html, server, watcher
