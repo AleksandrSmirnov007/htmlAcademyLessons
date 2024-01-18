0a3ddfb8705d556001d0e513fd10f9961933d045
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-01-18 03:55:37 +0300

    Кет энержи сборка проекта завершена. Далее  проверка по критериям

--- cat-energi-adaptiv/gulpfile.js
+++ cat-energi-adaptiv/gulpfile.js
@@ -1,3 +1,5 @@
+//  https://www.dev-notes.ru/articles/javascript-require-vs-import/
+
 import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
@@ -7,9 +9,11 @@ import autoprefixer from 'autoprefixer';
 import htmlmin from 'gulp-htmlmin';
 import terser from 'gulp-terser';
 import rename from 'gulp-rename';
-
 import csso from 'postcss-csso';
 import imagemin from 'gulp-imagemin';
+import webp from 'gulp-webp';
+import svgstore from 'gulp-svgstore';
+import {deleteAsync} from 'del'; // на npmjs.com написано так добавлять
 
 import sync from 'browser-sync';
 
@@ -55,11 +59,59 @@ export const optimizeImages = () => {
 }
 
 
-const copyImages = () => {
+export const copyImages = () => {
   return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(gulp.dest("build/img"))
 }
 
+// WebP
+
+export const createWebp = () => {
+  return gulp.src("source/img/**/*.{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"))
+}
+
+
+// Sprite
+
+ export const sprite = () => {
+  return gulp.src("source/img/sprites/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+}
+
+// Copy
+
+ export const copy = (done) => {
+  gulp.src([
+    "source/fonts/*.{woff2,woff}",
+    "source/*.ico",
+    "source/img/**/*.svg",
+    "source/*.webmanifest",
+    "!source/img/sprites/*.svg",
+  ], {
+    base: "source"
+  })
+    .pipe(gulp.dest("build"))
+  done();
+}
+
+// Clean
+
+export const clean = () => {
+  return deleteAsync("build");
+};
+
+// Reload
+
+const reload = (done) => {
+  sync.reload();
+  done();
+}
 
 // Server
 
@@ -79,10 +131,43 @@ const server = (done) => {
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', sync.reload);
+  gulp.watch("source/js/script.js", gulp.series(scripts));
+  // gulp.watch('source/*.html').on('change', sync.reload); // заменил эту строку из обновления от Кекса в разделе препроцессоров на нижнюю, так как при каждом изменении html-файлов нужно запускать теперь еше функцию минификации html.
+  gulp.watch("source/*.html", gulp.series(html, reload));
 }
 
 
+// Build
+
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
+);
+
+
+// Default
+
 export default gulp.series(
-  styles, server, watcher
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    styles,
+    html,
+    scripts,
+    sprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  )
 );
