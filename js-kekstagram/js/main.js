5c40d3cb3a80e797d9ff1a698b3f993291a8e5db
Aleksandr Smirnov <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-10-14 23:50:55 +0300

    Поправка домашнего задания (функуции случ число и длинна строки)
    Что бы в дальнейшем не путаться я привел код к тем же переменным что и у Кекса

--- js-kekstagram/js/main.js
+++ js-kekstagram/js/main.js
@@ -12,23 +12,43 @@
 
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomIntInclusive(a, b = 1) {
-  if (a === undefined) {
-    throw new Error('Первый параметр должен быть число, второй праметр по умолчанию равен 1');
-  }
+  // Чтобы не заставлять пользователя нашей функции помнить порядок аргументов,
+  // реализуем поддержку передачи минимального и максимального значения в любом порядке,
+  // а какое из них большее и меньшее вычислим с помощью Math.min и Math.max.
 
-  const min = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
-  const max = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
-  return Math.floor(Math.random() * (max - min + 1) + min); // Максимум и минимум включаются
+  // После нам нужно убедиться, что пользователь не передал дробные значения,
+  // для этого на всякий пожарный случай нижнюю границу диапазона
+  // мы округляем к ближайшему большему целому с помощью Math.ceil,
+  // а верхнюю границу - к ближайшему меньшему целому с помощью Math.floor
+
+  // Обратите внимание, чтобы учесть условие, что диапазон может быть [0, ∞),
+  // мы не ругаем пользователя за переданное отрицательное число,
+  // а просто берём его по модулю с помощью Math.abs
+
+  // Дальше используем Math.random() для получения случайного дробного числа в диапазоне [0, 1),
+  // которое домножаем на разницу между переданными числами плюс единица - это будет наша случайная дельта.
+  // После нужно сложить дельту с минимальным значением, чтобы получить итоговое случайное число.
+
+  // "Плюс единица", чтобы включить верхнюю границу диапазона в случайные числа
+
+  // И в конце с помощью метода Math.floor мы округляем полученный результат,
+  // потому что Math.random() генерирует только дробные числа и ноль.
+
+function getRandomPositiveInteger(a, b) {
+
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 }
 
-function stringLength(string, length) {
+function checkStringLength(string, length) {
   return string.length <= length;
 }
 
 
-getRandomIntInclusive(0, 1);
-stringLength('proba', 50);
+getRandomPositiveInteger(0, 1);
+checkStringLength('proba', 50);
 
 // если сервер(brausersinc) завис перезагрузить его cntl + c
 // при изменении кода в файле .js на странице в браузере(при работе brousersinc) появляется paused in debugger нажать на стрелку для перезагрузки.
