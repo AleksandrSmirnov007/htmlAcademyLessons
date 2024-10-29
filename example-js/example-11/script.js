f5f690200e1e4481154f953838453f3c7e1350d7
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-10-30 01:06:57 +0300

    Создание Помидоркогалереи

---
+++ example-js/example-11/script.js
@@ -0,0 +1,33 @@
+var pictures = [
+  'gallery-tomato/tomato-red-large.jpg',
+  'gallery-tomato/tomato-yellow-large.jpg',
+  'gallery-tomato/tomato-strange-large.jpg'
+];
+
+var thumbnails = document.querySelectorAll('.gallery__picture-preview');
+
+
+var fullPicture = document.querySelector('.full-picture');
+
+
+var addThumbnailClickHandler = function (thumbnail, picture) {
+  thumbnail.addEventListener('click', function () {
+    fullPicture.src = picture;
+  });
+}
+
+for (var i = 0; i < thumbnails.length; i++) {
+  addThumbnailClickHandler(thumbnails[i], pictures[i]);
+}
+
+/* Техническое задание
+
+Мяу! Нужно оживить галерею с фотографиями товаров.
+
+В галерее есть миниатюры (элементы с классом 'gallery__picture-preview') и большое изображение (класс 'full-picture').
+
+После клика по превью большая фотография должна поменяться. Большое изображение должно быть таким же, как и в миниатюре, только большего размера.
+
+Пути к полноразмерным фотографиям находятся в массиве pictures. Порядок элементов в массиве такой же, как и порядок миниатюр в разметке.
+
+*/
