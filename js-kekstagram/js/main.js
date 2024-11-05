3f399af3898ee653c0b3b16b0938ae734f4873f9
Aleksandr Smirnov <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-11-06 00:06:30 +0300

    рабрта над ошибками кексограмм

--- js-kekstagram/js/main.js
+++ js-kekstagram/js/main.js
@@ -1,5 +1,5 @@
 // Точка входа, основной модуль
-import { getPictures } from './data.js'
-import {renderPicture} from './picture.js'
+import { getPictures } from './data.js' // импортироуем функцию создания данных объектов
+import {renderPicture} from './picture.js' // импортируем функцию превращения данных в элементы и добавление на страницу
 
-renderPicture(getPictures());
+renderPicture(getPictures()); // читаем изнутри: создаем данные объектов картинок, а далее их превращаем в элементы и отрисовываем
--- js-kekstagram/js/picture.js
+++ js-kekstagram/js/picture.js
@@ -1,22 +1,38 @@
-const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictureTemplate = document
+  .querySelector('#picture')
+  .content.querySelector('.picture'); // ищем элемент template  тут же берем его контент и тут же ищем по классу захавтываем родителя
 
 const container = document.querySelector('.pictures');  // сюда будем складывать картинки пользователей
 
-const renderPicture = (userPicturesList) => {
-  const fragment = document.createDocumentFragment();
+// реструктуризация {comments, description, likes, url} переданный обьект раскидается на параметры
 
-  userPicturesList.forEach((userPicturesItem) => {
 
-    const pictureItem = pictureTemplate.cloneNode(true);
+// функция для создания элемента на основе обьекта из данных
+const createPicture = ({comments, description, likes, url}) => {
 
-    pictureItem.querySelector('img').src = userPicturesItem.url;
-    pictureItem.querySelector('.picture__likes').textContent = userPicturesItem.likes;
-    pictureItem.querySelector('.picture__comments').textContent = userPicturesItem.comments.length;
+  const picture = pictureTemplate.cloneNode(true);  // делаем клон с шаблона
 
-    fragment.appendChild(pictureItem);
-  });
+  picture.querySelector('.picture__img').src = url;  // ищем свойтсво src у элемента в клоне и добавляем в него данные из переданного в параметр обьекта
+  picture.querySelector('.picture__img').alt = description;;  // ищем свойтсво alt у элемента в клоне и добавляем в него данные из переданного в параметр обьекта
+  picture.querySelector('.picture__likes').textContent = likes;  // ищем другой уже класс в клоне и записываем в текстовый коетент данные из переданного обьекта
+  picture.querySelector('.picture__comments').textContent = comments.length; // тоже ищем подходящий класс но в это раз передаем количество элементов в массиве коментариев
 
-  container.appendChild(fragment);
+  return picture; // возращаем наш полученный элемент (модифицированный клон) Фунция проста принимает в параметр обьект в виде данных а возвращает по сути уже элемент
 };
 
+
+// создаем фунцию которая примет в параметр массив обьектов и отрисует его на странице
+const renderPicture = (pictures) => {
+  const fragment = document.createDocumentFragment(); // создаем коробку
+
+
+  // перебираем последовательно каждый элемент-объект  массива с данными
+  pictures.forEach((picture) => {
+    const pictureElement = createPicture(picture);  // передаем объект с данными а на выходе получаем элемент и записываем его в переменную
+    fragment.appendChild(pictureElement); // добавляем элемент в коробку и так со всеми элементами
+  });
+  container.appendChild(fragment); // добавляем накопившиеся элементы сразу и в нужное место
+};
+
+// готово можно экспортировать главную функцию
 export {renderPicture};
