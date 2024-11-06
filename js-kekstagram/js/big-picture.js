801e333ec5fc6f2d07c791d47d62cc86a2cac7df
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-11-06 22:52:36 +0300

    В процессе выполнения задания 7.11. Отрисуй меня полностью (часть 2)

--- js-kekstagram/js/big-picture.js
+++ js-kekstagram/js/big-picture.js
@@ -1,9 +1,19 @@
 const bigPicture = document.querySelector('.big-picture');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
+// const pictures = document.querySelectorAll('.picture');
 
+const pictureContainer = document.querySelector('.pictures');
+
+console.log(pictureContainer.children);
+
+const pictures = pictureContainer.children;
+
+
+console.log(pictures[3]);
 
 console.log('big-picture подключен');
-bigPicture.classList.remove('hidden');
+// bigPicture.classList.remove('hidden');
+
 
 closeButton.addEventListener('click', function () {
   bigPicture.classList.add('hidden');
--- js-kekstagram/js/main.js
+++ js-kekstagram/js/main.js
@@ -2,6 +2,41 @@
 import { getPictures } from './data.js' // импортироуем функцию создания данных объектов
 import {renderPicture} from './picture.js' // импортируем функцию превращения данных в элементы и добавление на страницу
 
+// import './big-picture.js';
+
 renderPicture(getPictures()); // читаем изнутри: создаем данные объектов картинок, а далее их превращаем в элементы и отрисовываем
 
-import './big-picture.js';
+const bigPicture = document.querySelector('.big-picture');
+
+const bigPictureImage = bigPicture.querySelector('.big-picture__img').children[0]; // у элемента img нет класса и находим его как первого (и едимтвенного) ребенка .big-picture__img'
+
+console.log(bigPictureImage);
+const closeButton = bigPicture.querySelector('.big-picture__cancel');
+
+const pictureContainer = document.querySelector('.pictures');
+
+// pictures.forEach(element => {
+//   console.log(element);
+// });
+// Непонятно почему forEach не работает на коллекции элементов!!!????
+// a вот for работает
+
+const pictures = pictureContainer.querySelectorAll('.picture');
+
+console.log(bigPicture);
+
+for (const el of pictures) {
+
+  el.addEventListener('click', function () {
+    bigPicture.classList.remove('hidden');
+    const imageEl = el.querySelector('.picture__img');
+    console.log(el);
+    bigPictureImage.src = imageEl.src;
+  });
+};
+
+console.log('big-picture подключен');
+
+closeButton.addEventListener('click', function () {
+  bigPicture.classList.add('hidden');
+});
