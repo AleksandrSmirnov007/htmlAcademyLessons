2e96a52672c3641c2fd700f8feb0ca5966bf4d38
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-11-24 13:44:28 +0300

    Валидация формы загрузки изображения
    добавление обработчиков на форму при открытии формы и удаление обработчика submit при скрытии формы

--- js-kekstagram/js/form-upload.js
+++ js-kekstagram/js/form-upload.js
@@ -1,4 +1,4 @@
-alert('файл form-upload подключен');
+console.log('form-upload.js is working');
 
 const formUpload = document.querySelector('.img-upload__form');
 const imgUploadOverlay = formUpload.querySelector('.img-upload__overlay');
@@ -7,10 +7,16 @@ const uploadCancel = formUpload.querySelector('.img-upload__cancel');
 const body = document.querySelector('body');
 
 
+const addSubmitDisabled = () => {
+  const submit = formUpload.querySelector("input[type=submit], button[type=submit]");
+  submit.disabled = true;
+}
+
 const hideUploadForm = () => {
   imgUploadOverlay.classList.add('hidden');
   body.classList.remove('modal-open');
   document.removeEventListener('keydown', onEscKeyDownForm);
+  formUpload.removeEventListener('submit', addSubmitDisabled);
 }
 
 function onEscKeyDownForm(evt) {
@@ -22,14 +28,23 @@ function onEscKeyDownForm(evt) {
 
 const addListenerUploadCancel = () => uploadCancel.addEventListener('click', hideUploadForm);
 
+
+formUpload.addEventListener('submit', addSubmitDisabled);
+
+
 const showUploadForm = () => {
   imgUploadOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
   addListenerUploadCancel();
   document.addEventListener('keydown', onEscKeyDownForm);
+  formUpload.addEventListener('submit', addSubmitDisabled);
 }
 
 const addListenerUploadInput = () => imgUploadInput.addEventListener('change', showUploadForm);
 
 
-export {showUploadForm, addListenerUploadInput};
+// new Pristine(formUpload);
+
+
+
+export {addListenerUploadInput};
--- js-kekstagram/js/main.js
+++ js-kekstagram/js/main.js
@@ -2,7 +2,7 @@ import { getPictures } from './data.js';
 import { renderPictures} from './picture.js';
 
 // import './form-upload.js';
-import {showUploadForm, addListenerUploadInput} from './form-upload.js';
+import { addListenerUploadInput} from './form-upload.js';
 
 
 renderPictures(getPictures());
