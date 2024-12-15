2b76d1f8ed0a87370a51d45b651ccb379ae39ec6
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-12-15 23:28:31 +0300

    Кексограм работа над  ошибками  с noUsSlider завершена

---
+++ js-kekstagram/js/trashcan-for-my-js/upload-slider.js
@@ -0,0 +1,200 @@
+console.log('upload-slider is working');
+// Для эффекта «Оригинал» CSS-стили filter удаляются.
+// Для эффекта «Хром» — filter: grayscale(0..1) с шагом 0.1;
+// Для эффекта «Сепия» — filter: sepia(0..1) с шагом 0.1;
+// Для эффекта «Марвин» — filter: invert(0..100%) с шагом 1%;
+// Для эффекта «Фобос» — filter: blur(0..3px) с шагом 0.1px;
+// Для эффекта «Зной» — filter: brightness(1..3) с шагом 0.1;
+
+const FILTERS = [
+  {
+    name: 'none',
+    style: 'none',
+    min: 0,
+    max: 1,
+    step: 1,
+    unit: '',
+  },
+
+  {
+    name: 'chrome',
+    style: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    start: 1,
+    unit: '',
+  },
+
+  {
+    name: 'sepia',
+    style: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    start: 1,
+    unit: '',
+  },
+
+  {
+    name: 'marvin',
+    style: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    start: 100,
+    unit: '%',
+  },
+
+  {
+    name: 'phobos',
+    style: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    start: 3,
+    unit: 'px',
+  },
+
+  {
+    name: 'heat',
+    style: 'brightness',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    start: 3,
+    unit: '',
+  },
+];
+
+const FILTERS_INDEX_MAP = [
+  'none', 'chrome', 'sepia', 'marvin', 'phobos', 'heat',
+];
+
+const image = document.querySelector('.img-upload__preview img');
+const effectLevelFieldset = document.querySelector('.effect-level');
+const sliderElement = effectLevelFieldset.querySelector('.effect-level__slider');
+const valueElement = effectLevelFieldset.querySelector('.effect-level__value');
+const effectList = document.querySelector('.effects__list');
+
+let CURRENT_FILTER = 'none';
+let filterIndex = 0;
+
+// Для разработки открытие скрытого поля
+// valueElement.style.display = 'inline-block';
+// valueElement.style.color = 'black';
+////////////
+
+effectLevelFieldset.classList.add('hidden');
+
+const effectsList = document.querySelector('.effects__list');
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 1,
+  },
+  step: 1,
+  start: 1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0); // если нужен вывод к примеру в процентах то можно использовать шаблонную строку return `${value.toFixed(0)} %`;
+      } else{
+      return value.toFixed(1);
+      }
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+
+// 'элемент бегунка слайдера появляется только после создания библиотекой слайдера
+const noUiHandle = document.querySelector('.noUi-handle');
+
+console.log(noUiHandle);
+
+sliderElement.noUiSlider.on('update', () => {
+// модификаторы классов для ползунка в css = = = =   .noUi-handle--min для сгругления левой части,  noUi-handle--max для скругления правой части
+  valueElement.value = sliderElement.noUiSlider.get();
+  if (valueElement.value == FILTERS[filterIndex].min) {
+    noUiHandle.classList.add('noUi-handle--min');
+  } else {
+    noUiHandle.classList.remove('noUi-handle--min');
+  }
+
+  if (valueElement.value == FILTERS[filterIndex].max) {
+    noUiHandle.classList.add('noUi-handle--max');
+  } else {
+    noUiHandle.classList.remove('noUi-handle--max');
+  }
+
+  const unit =  FILTERS[filterIndex].unit;
+  let filterCssValue = `${FILTERS[filterIndex].style}(${valueElement.value}${unit})`;
+  image.style.filter = filterCssValue;
+  const style = window.getComputedStyle(image);
+  const filter = style.getPropertyValue('filter');
+
+});
+
+const findFilterIndex = (filter) => {
+  for (let i = 0;  i < FILTERS_INDEX_MAP.length; i++) {
+    if (FILTERS_INDEX_MAP[i] === filter) {
+      return i;
+    }
+  }
+}
+
+effectList.addEventListener('change', (evt) => {
+
+  CURRENT_FILTER = evt.target.value;  // забираем содержимое value от input type='radio' на который кликнули
+
+  if(CURRENT_FILTER === 'none'){
+    effectLevelFieldset.classList.add('hidden');
+    image.style.filter = 'none';
+  } else {
+    effectLevelFieldset.classList.remove('hidden');
+  }
+  image.className = ''; // присваиваем имени класса пустую строку тем самым удаляем все классы если и был уже присвоен клас другого эффекта он удалится
+  image.classList.add(`effects__preview--${CURRENT_FILTER}`); // добавляем неоходимый класс методом составления шаблонной строки и добавляя имя фильра
+
+  filterIndex = findFilterIndex(CURRENT_FILTER);
+
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: FILTERS[filterIndex].min,
+      max: FILTERS[filterIndex].max,
+    },
+    step: FILTERS[filterIndex].step,
+    start: FILTERS[filterIndex].start,
+  }
+  );
+});
+
+const resetSlider = () => {
+  image.className = '';
+  image.style.filter = 'none';
+};
+
+export {resetSlider};
+
+
+
+
+// 2.2. Наложение эффекта на изображение:
+
+// По умолчанию должен быть выбран эффект «Оригинал».
+// На изображение может накладываться только один эффект.
+// При смене эффекта, выбором одного из значений среди радиокнопок .effects__radio, добавить картинке внутри .img-upload__preview CSS-класс, соответствующий эффекту. Например, если выбран эффект .effect-chrome, изображению нужно добавить класс effects__preview--chrome.
+// Интенсивность эффекта регулируется перемещением ползунка в слайдере. Слайдер реализуется сторонней библиотекой для реализации слайдеров noUiSlider. Уровень эффекта записывается в поле .effect-level__value. При изменении уровня интенсивности эффекта (предоставляется API слайдера), CSS-стили картинки внутри .img-upload__preview обновляются следующим образом:
+// Для эффекта «Хром» — filter: grayscale(0..1) с шагом 0.1;
+// Для эффекта «Сепия» — filter: sepia(0..1) с шагом 0.1;
+// Для эффекта «Марвин» — filter: invert(0..100%) с шагом 1%;
+// Для эффекта «Фобос» — filter: blur(0..3px) с шагом 0.1px;
+// Для эффекта «Зной» — filter: brightness(1..3) с шагом 0.1;
+// Для эффекта «Оригинал» CSS-стили filter удаляются.
+// При выборе эффекта «Оригинал» слайдер скрывается.
+// При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%): слайдер, CSS-стиль изображения и значение поля должны обновляться.
--- js-kekstagram/js/upload-slider.js
+++ js-kekstagram/js/upload-slider.js
@@ -1,191 +1,5 @@
 console.log('upload-slider is working');
-// Для эффекта «Оригинал» CSS-стили filter удаляются.
-// Для эффекта «Хром» — filter: grayscale(0..1) с шагом 0.1;
-// Для эффекта «Сепия» — filter: sepia(0..1) с шагом 0.1;
-// Для эффекта «Марвин» — filter: invert(0..100%) с шагом 1%;
-// Для эффекта «Фобос» — filter: blur(0..3px) с шагом 0.1px;
-// Для эффекта «Зной» — filter: brightness(1..3) с шагом 0.1;
-
-const FILTERS = [
-  {
-    name: 'none',
-    style: 'none',
-    min: 0,
-    max: 1,
-    step: 1,
-    unit: '',
-  },
-
-  {
-    name: 'chrome',
-    style: 'grayscale',
-    min: 0,
-    max: 1,
-    step: 0.1,
-    start: 1,
-    unit: '',
-  },
-
-  {
-    name: 'sepia',
-    style: 'sepia',
-    min: 0,
-    max: 1,
-    step: 0.1,
-    start: 1,
-    unit: '',
-  },
-
-  {
-    name: 'marvin',
-    style: 'invert',
-    min: 0,
-    max: 100,
-    step: 1,
-    start: 100,
-    unit: '%',
-  },
-
-  {
-    name: 'phobos',
-    style: 'blur',
-    min: 0,
-    max: 3,
-    step: 0.1,
-    start: 3,
-    unit: 'px',
-  },
-
-  {
-    name: 'heat',
-    style: 'brightness',
-    min: 0,
-    max: 3,
-    step: 0.1,
-    start: 3,
-    unit: '',
-  },
-];
-
-const FILTERS_INDEX_MAP = [
-  'none', 'chrome', 'sepia', 'marvin', 'phobos', 'heat',
-];
-
-const imgUploadPreview = document.querySelector('.img-upload__preview img');
-const effectLevelFieldset = document.querySelector('.effect-level');
-const sliderElement = effectLevelFieldset.querySelector('.effect-level__slider');
-const valueElement = effectLevelFieldset.querySelector('.effect-level__value');
-const effectList = document.querySelector('.effects__list');
-
-let CURRENT_FILTER = 'none';
-let filterIndex = 0;
-
-// Для разработки открытие скрытого поля
-// valueElement.style.display = 'inline-block';
-// valueElement.style.color = 'black';
-////////////
-
-effectLevelFieldset.classList.add('hidden');
-
-const effectsList = document.querySelector('.effects__list');
-
-noUiSlider.create(sliderElement, {
-  range: {
-    min: 0,
-    max: 1,
-  },
-  step: 1,
-  start: 1,
-  connect: 'lower',
-  format: {
-    to: function (value) {
-      if (Number.isInteger(value)) {
-        return value.toFixed(0); // если нужен вывод к примеру в процентах то можно использовать шаблонную строку return `${value.toFixed(0)} %`;
-      } else{
-      return value.toFixed(1);
-      }
-    },
-    from: function (value) {
-      return parseFloat(value);
-    },
-  },
-});
-
-
-// 'элемент бегунка слайдера появляется только после создания библиотекой слайдера
-const noUiHandle = document.querySelector('.noUi-handle');
-
-console.log(noUiHandle);
-
-sliderElement.noUiSlider.on('update', () => {
-// модификаторы классов для ползунка в css = = = =   .noUi-handle--min для сгругления левой части,  noUi-handle--max для скругления правой части
-  valueElement.value = sliderElement.noUiSlider.get();
-  if (valueElement.value == FILTERS[filterIndex].min) {
-    noUiHandle.classList.add('noUi-handle--min');
-  } else {
-    noUiHandle.classList.remove('noUi-handle--min');
-  }
-
-  if (valueElement.value == FILTERS[filterIndex].max) {
-    noUiHandle.classList.add('noUi-handle--max');
-  } else {
-    noUiHandle.classList.remove('noUi-handle--max');
-  }
-
-  const unit =  FILTERS[filterIndex].unit;
-  let filterCssValue = `${FILTERS[filterIndex].style}(${valueElement.value}${unit})`;
-  imgUploadPreview.style.filter = filterCssValue;
-  const style = window.getComputedStyle(imgUploadPreview);
-  const filter = style.getPropertyValue('filter');
-
-});
-
-const findFilterIndex = (filter) => {
-  for (let i = 0;  i < FILTERS_INDEX_MAP.length; i++) {
-    if (FILTERS_INDEX_MAP[i] === filter) {
-      return i;
-    }
-  }
-}
-
-effectList.addEventListener('change', (evt) => {
-
-  CURRENT_FILTER = evt.target.value;  // забираем содержимое value от input type='radio' на который кликнули
-
-  if(CURRENT_FILTER === 'none'){
-    effectLevelFieldset.classList.add('hidden');
-    imgUploadPreview.style.filter = 'none';
-  } else {
-    effectLevelFieldset.classList.remove('hidden');
-  }
-  imgUploadPreview.className = ''; // присваиваем имени класса пустую строку тем самым удаляем все классы если и был уже присвоен клас другого эффекта он удалится
-  imgUploadPreview.classList.add(`effects__preview--${CURRENT_FILTER}`); // добавляем неоходимый класс методом составления шаблонной строки и добавляя имя фильра
-
-  filterIndex = findFilterIndex(CURRENT_FILTER);
-
-  sliderElement.noUiSlider.updateOptions({
-    range: {
-      min: FILTERS[filterIndex].min,
-      max: FILTERS[filterIndex].max,
-    },
-    step: FILTERS[filterIndex].step,
-    start: FILTERS[filterIndex].start,
-  }
-  );
-});
-
-const resetSlider = () => {
-  imgUploadPreview.className = '';
-  imgUploadPreview.style.filter = 'none';
-};
-
-export {resetSlider};
-
-
-
-
 // 2.2. Наложение эффекта на изображение:
-
 // По умолчанию должен быть выбран эффект «Оригинал».
 // На изображение может накладываться только один эффект.
 // При смене эффекта, выбором одного из значений среди радиокнопок .effects__radio, добавить картинке внутри .img-upload__preview CSS-класс, соответствующий эффекту. Например, если выбран эффект .effect-chrome, изображению нужно добавить класс effects__preview--chrome.
@@ -198,3 +12,162 @@ export {resetSlider};
 // Для эффекта «Оригинал» CSS-стили filter удаляются.
 // При выборе эффекта «Оригинал» слайдер скрывается.
 // При переключении эффектов, уровень насыщенности сбрасывается до начального значения (100%): слайдер, CSS-стиль изображения и значение поля должны обновляться.
+const image = document.querySelector('.img-upload__preview img');
+const sliderElement = document.querySelector('.effect-level__slider');
+const effectLevel = document.querySelector('.effect-level__value');
+const form = document.querySelector('.img-upload__form');
+
+// Для разработки открытие скрытого поля
+effectLevel.style.display = 'inline-block';
+effectLevel.style.color = 'black';
+////////////
+
+const EFFECTS = [
+  {
+    name: 'none',
+    min: 0,
+    max: 100,
+    step: 1,
+  },
+
+  {
+    name: 'chrome',
+    style: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+
+  {
+    name: 'sepia',
+    style: 'sepia',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+  },
+
+  {
+    name: 'marvin',
+    style: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+  },
+
+  {
+    name: 'phobos',
+    style: 'blur',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: 'px',
+  },
+
+  {
+    name: 'heat',
+    style: 'brightness',
+    min: 0,
+    max: 3,
+    step: 0.1,
+    unit: '',
+  },
+];
+
+const DEFAULT_EFFECT = EFFECTS[0];
+let chosenEffect = DEFAULT_EFFECT;
+
+const isDefault = () => chosenEffect == DEFAULT_EFFECT;
+
+
+const updateSlider = () => {
+  sliderElement.classList.remove('hidden');
+
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: chosenEffect.min,
+      max: chosenEffect.max,
+    },
+    step: chosenEffect.step,
+    start: chosenEffect.max,
+  }
+  );
+
+  if(isDefault()) {
+    sliderElement.classList.add('hidden');
+  }
+};
+
+
+const onFormChange = (evt) => {
+  if(!evt.target.classList.contains('effects__radio')) {
+    return; // если цель клика не содержит класс effects__radio то верни "ничего" и просто заверши функцию и выполняй следующий
+  }
+  chosenEffect = EFFECTS.find((effect) => effect.name === evt.target.value); // find()Метод Array экземпляров возвращает первый элемент в предоставленном массиве, который удовлетворяет предоставленной функции тестирования. Если никакие значения не удовлетворяют функции тестирования, возвращается undefined. Если вам нужен индекс найденного элемента в массиве, используйте findIndex().
+  updateSlider();
+  console.log(chosenEffect);
+}
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: DEFAULT_EFFECT.min,
+    max: DEFAULT_EFFECT.max,
+  },
+  step: DEFAULT_EFFECT.step,
+  start: DEFAULT_EFFECT.max,
+  connect: 'lower',
+});
+updateSlider();
+
+// 'элемент бегунка слайдера появляется только после создания библиотекой слайдера
+const noUiHandle = document.querySelector('.noUi-handle');
+
+
+// в архиве проекта нет этого кода но радиус бегунку добавляется в крайних положениях
+const addRadiusHandle = (value) => {
+  // модификаторы классов для ползунка в css = = = =   .noUi-handle--min для сгругления левой части,  noUi-handle--max для скругления правой части
+  if (value == chosenEffect.min) {
+    noUiHandle.classList.add('noUi-handle--min');
+  } else {
+    noUiHandle.classList.remove('noUi-handle--min');
+  }
+
+  if (value == chosenEffect.max) {
+    noUiHandle.classList.add('noUi-handle--max');
+  } else {
+    noUiHandle.classList.remove('noUi-handle--max');
+  }
+};
+
+const onSliderUpdate = () => {
+  // похоже на блок подготовки к значению фильтра none (оригинал)
+  image.style.filter = ''; // обнуляем фильр в архиве проекта так написано, хотя не пойму зачем постоянно при движении бегунка обновлять фильтр
+  image.className = '' // удаляем все классы ранее хотя тоже через чур часто ведь классы меняются только при выборе эффекта зачем их чистить имеено в этом месте при каждом перемещении ползунка
+  effectLevel.value = ''; // удаляем значение поля и каждый раз перезаписываем новое (ну наверно актуально)
+  if(isDefault()) {
+    return; // chosenEffect == DEFAULT_EFFECT; завершит выполнение и выйдет из функции
+  }
+
+  console.log(window.getComputedStyle(image).getPropertyValue('filter'));
+  const sliderValue = sliderElement.noUiSlider.get();
+  image.style.filter = `${chosenEffect.style}(${sliderValue}${chosenEffect.unit})`;
+  effectLevel.value = sliderValue;
+
+  addRadiusHandle(effectLevel.value);
+};
+
+const resetSlider = () => {
+  image.className = '';
+  image.style.filter = 'none';
+};
+
+form.addEventListener('change', onFormChange);
+sliderElement.noUiSlider.on('update', onSliderUpdate);
+
+export {resetSlider};
+
+
+
+
