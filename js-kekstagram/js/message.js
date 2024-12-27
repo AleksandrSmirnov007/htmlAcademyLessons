0ea7456710eac2295385bfac6eace3c3726be834
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2024-12-27 19:57:24 +0300

    Кексограм работа над ошибками
    доработать модуль message - так как не работает скрытие сообщения по клику на body

--- js-kekstagram/js/message.js
+++ js-kekstagram/js/message.js
@@ -1,3 +1,46 @@
 const successMessage = document.querySelector('#success').content.querySelector('.success');
-
 const errorMessage = document.querySelector('#error').content.querySelector('.error');
+const body = document.querySelector('body');
+
+const showSuccessMessage = () => {
+  body.append(successMessage);
+  body.addEventListener('keydown', onEscDown);
+  body.addEventListener('click', onBodyClick);
+  successMessage
+    .querySelector('.success__button')
+    .addEventListener('click', hideMessage);
+};
+
+const showErrorMessage = () => {
+  body.append(errorMessage);
+  body.addEventListener('keydown', onEscDown);
+  errorMessage
+    .querySelector('.error__button')
+    .addEventListener('click', hideMessage);
+};
+
+
+function hideMessage () {
+  const messageElement = document.querySelector('.success') || document.querySelector('.error');
+  messageElement.remove();
+  body.removeEventListener('keydown', onEscDown);
+  body.removeEventListener('click', onBodyClick);
+};
+
+function onBodyClick (evt) {
+  if (evt.target.closest('.error__inner') || evt.target.closest('.success__inner')) {
+    return; // приведет к выходу из фунции без каких либо действий если мы кликнем на содержимое сообщения
+  }
+  hideMessage(); // если кликнем на другое место в body кроме содержимого сообщение то сообщение скроется
+};
+
+function onEscDown (evt) {
+  if(evt.key === 'Escape') {
+    evt.preventDefault();
+    evt.stopPropagation(); //Метод stopPropagation() объекта Event в JavaScript предотвращает дальнейшую передачу текущего события (предотвращает всплытие по дереву DOM)
+    hideMessage();
+  }
+}
+
+showErrorMessage()
+export {showSuccessMessage, showErrorMessage};
