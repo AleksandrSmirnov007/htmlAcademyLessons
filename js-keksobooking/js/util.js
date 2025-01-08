115ca9b98b9dfb8fcb67aadeea7740141d81d0a6
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2025-01-09 00:13:40 +0300

    Кексобукинг генерация данных начало

--- js-keksobooking/js/util.js
+++ js-keksobooking/js/util.js
@@ -1,6 +1,18 @@
-const getRandomPositiveFloat = (a, b) => {
+const getRandomPositiveFloat = (a, b, digits = 1) => {
+  const lower = Math.min(Math.abs(a), Math.abs(b));
+  const upper = Math.max(Math.abs(a), Math.abs(b));
+  const result = (upper - lower) * Math.random() + lower;
+  return result.toFixed(digits);
+};
+
+const getRandomPostiveInteger = (a, b) => {
   const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
   const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
   const result = (upper - lower + 1) * Math.random() + lower;
   return Math.floor(result);
 };
+
+const getRandomArrayElement = (array) =>
+  array[getRandomPostiveInteger(0, array.length - 1)];
+
+export {getRandomPositiveFloat};
