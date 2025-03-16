b440c0e90e9614aa7f85511f6743e021483c3f5b
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2025-03-16 20:31:27 +0300

    learn python

---
+++ PYTHON/main.py
@@ -0,0 +1,114 @@
+import turtle
+
+
+# square = turtle.Turtle()
+# square.penup()
+# square.goto(-200,-50)
+# square.pendown()
+
+# square.shape("turtle")
+# square.color("red" , "red")
+# square.begin_fill()
+# for i in range(5):
+#     square.forward(350)
+#     square.right(144)
+
+# square.end_fill()
+# turtle.exitonclick()
+
+
+turtle.penup()
+turtle.goto(-350,-50)
+turtle.pendown()
+
+# turtle.shape("turtle")
+turtle.color("red" , "red")
+# turtle.begin_fill()
+# for i in range(5):
+turtle.begin_fill()
+
+turtle.right(270)
+turtle.forward(130)
+turtle.right(135)
+turtle.forward(70)
+turtle.left(90)
+turtle.forward(70)
+turtle.right(135)
+turtle.forward(130)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(80)
+turtle.left(135)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+turtle.left(135)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(20)
+
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(-220, -50)
+turtle.pendown()
+
+turtle.begin_fill()
+
+turtle.right(110)
+turtle.forward(140)
+turtle.right(140)
+turtle.forward(140)
+turtle.right(110)
+turtle.forward(15)
+turtle.right(70)
+turtle.forward(95)
+turtle.right(220)
+turtle.forward(95)
+turtle.right(70)
+turtle.forward(15)
+turtle.right(110)
+turtle.forward(50)
+turtle.right(70)
+turtle.forward(60)
+turtle.right(70)
+turtle.forward(15)
+turtle.right(110)
+turtle.forward(70)
+
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(-90, -50)
+turtle.pendown()
+
+turtle.begin_fill()
+
+turtle.right(90)
+turtle.forward(130)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(145)
+turtle.forward(60)
+turtle.right(55)
+turtle.forward(20)
+turtle.right(125)
+turtle.forward(80)
+turtle.left(70)
+turtle.forward(78)
+turtle.right(125)
+turtle.forward(20)
+turtle.right(55)
+turtle.forward(55)
+turtle.left(145)
+turtle.forward(45)
+turtle.right(90)
+turtle.forward(25)
+
+turtle.end_fill()
+
+
+turtle.exitonclick()
