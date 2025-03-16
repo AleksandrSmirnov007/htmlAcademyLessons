a6992e82623759e336f73d82ece39cc933e4ef21
Aleksandr Smirnov <115838241+AleksandrSmirnov007@users.noreply.github.com>
2025-03-17 02:37:01 +0300

    Работа с localStorage

--- example-js-new-1/example-33/js/main.js
+++ example-js-new-1/example-33/js/main.js
@@ -1,3 +1,5 @@
+// https://htmlacademy.ru/blog/js/localstorage
+
 import './other.js';
 console.log('проба');
 
@@ -11,3 +13,37 @@ const foo = JSON.parse(
 console.log(foo);
 
 console.log( JSON.stringify(localStorage, null, 2) );
+
+const saveInput = document.querySelector('.input');
+console.log(saveInput);
+
+const saveButton = document.querySelector('.button');
+console.log(saveButton);
+
+const showButton = document.querySelector('.show-button');
+console.log(showButton);
+
+const saveData = (data) => {
+  console.log('результат работы cachedData');
+  const cachedData = localStorage.getItem('someData');
+
+  console.log(cachedData);
+  if (cachedData) {
+    console.log('данные есть');
+    console.log(JSON.parse(cachedData));
+    localStorage.setItem('someData', JSON.stringify(data));
+  } else {
+    console.log('данных нет');
+    localStorage.setItem('someData', JSON.stringify(data));
+  }
+
+}
+
+saveButton.addEventListener('click', () => {
+  console.log(saveInput.value);
+  saveData(saveInput.value);
+});
+
+showButton.addEventListener('click', () => {
+  console.log(JSON.parse(localStorage.getItem('someData')));
+});
