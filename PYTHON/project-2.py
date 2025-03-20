90f37e1e08657149b84b19661bbfc347e2888035
ваше имя <115838241+AleksandrSmirnov007@users.noreply.github.com>
2025-03-20 20:11:19 +0300

    Максим - python

--- PYTHON/project-2.py
+++ PYTHON/project-2.py
@@ -1,22 +1,16 @@
 import pygame
-import math
+import time
 
 pygame.init()
-screen_width = 500
+screen_width = 1000
 screen_height = 500
 screen = pygame.display.set_mode(( screen_width, screen_height))
-pygame.display.set_caption("Геометрические фигуры")
-
-# задаем название окна
-pygame.display.set_caption("Белый фон")
-
 
 background_color = (255, 255, 255)
 
 screen.fill(background_color)
 
 # рисуем прямоугольник
-
 black = (0, 0, 0)
 
 rect_x = 50
@@ -28,7 +22,22 @@ pygame.draw.rect(screen, black, (rect_x, rect_y, rect_width, rect_height))
 pygame.display.flip()
 
 # показываем окно, пока пользователь не нажмет кнопку "Закрыть"
+
 while True:
+  screen.fill(background_color)
+  if rect_x <= 200:
+    rect_x = rect_x + 1
+  if rect_x > 200:
+    rect_y = rect_y + 1
+  if rect_y > 200:
+    rect_x = rect_x - 2
+  if rect_x < 0:
+    rect_y = rect_y - 2
+  time.sleep(0.001)
+
+  pygame.draw.rect(screen, black, (rect_x, rect_y, rect_width, rect_height))
+  pygame.display.flip()
+
   for ivent in pygame.event.get():
     if ivent.type == pygame.QUIT:
       pygame.quit()
